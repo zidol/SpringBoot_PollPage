6a524101469f8e922b5f22fa071308f4430acc4f
zidol <ajh3166@naver.com>
2019-08-28 21:41:07 +0900

    AuthenticationEntryPoint 주석 추가

--- src/main/java/com/example/zidols/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/zidols/security/JwtAuthenticationEntryPoint.java
@@ -11,6 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * 인증이 필요한 자원에 접근하려고 시도하는 인증되지 않은 사용자에게 예외를 발생시킬 때마다 해당 메소드 호출
+ * 예외 메시지 401 오류 보여줌
+ */
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
