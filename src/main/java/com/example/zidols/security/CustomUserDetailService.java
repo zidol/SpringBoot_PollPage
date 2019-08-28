1a61ce4ab77b8234c4210dab96604d1e012f2fea
zidol <ajh3166@naver.com>
2019-08-28 21:42:31 +0900

    사용자 정의 SpringSecurity UserDetailService

---
+++ src/main/java/com/example/zidols/security/CustomUserDetailService.java
@@ -0,0 +1,42 @@
+package com.example.zidols.security;
+
+import com.example.zidols.model.User;
+import com.example.zidols.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * UserDetailService 인터페이스로 사용자 이름이 주어지면, 해당 사용자의 데이터를 로드하는
+ * CustomUserDetailService 클래스
+ */
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        // Let people login with either username or email
+        User user  = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail)
+                );
+        return UserPrincipal.create(user);
+    }
+    // This method is used by JWTAithenticationFilter
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with id : " + id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
